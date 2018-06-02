be0e8c13ad2e2dd2f1e42d185cc245b2de0a4c7f
fscarpello <scarpello.franco@gmail.com>
2018-06-02 16:14:18 -0300

    Implementación de método saludar

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,8 +1,17 @@
 package loom.hola_mundo;
 
+/*
+ * Clase que saluda.
+ * Saluda a la persona indicada o genéricamente si no se indica.
+ */
+
 public class HolaMundo {
 
+	/**
+	 * @param persona: recibe el nombre de la persona a saludar
+	 * @return String con saludo.
+	 */
     public String saludar(final String persona) {
-        throw new RuntimeException("¡Implementar este método!");
+    	return persona != "" ? "¡Hola, " + persona + "!" : "¡Hola!";
     }
 }
--- src/test/java/loom/hola_mundo/HolaMundoTests.java
+++ src/test/java/loom/hola_mundo/HolaMundoTests.java
@@ -1,12 +1,29 @@
 package loom.hola_mundo;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class HolaMundoTests {
-    @Test
+    
+	HolaMundo holaMundo;
+	
+	// Antes de ejecutar cada test instancio un nuevo objeto HolaMundo.
+	@Before
+	public void setup() {
+		holaMundo = new HolaMundo();		
+	}
+	
+	// Saludo básico
+	@Test
     public void queSaludaCorrectamente() {
-        HolaMundo holaMundo = new HolaMundo();
         Assert.assertEquals("¡Hola, Kent Beck!", holaMundo.saludar("Kent Beck"));
     }
+	
+    // Saludos con persona no indicada.
+    @Test
+    public void queSaludaGenerico() {
+        Assert.assertEquals("¡Hola!", holaMundo.saludar(""));
+    }
+	
 }
